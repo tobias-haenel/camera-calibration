756f9085230c6177cc3e98b7f5f2dde997add072
Tobias Hänel <contact@tobias-haenel.de>
2017-08-28 17:41:47 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int
+main() {
+    // read settings file
+
+    // perform camera calibration
+        // get images
+        // find features
+        // extract 2D Positions
+        // calculate 3D object positions
+        // start calculation
+
+    // extract intrinsic parameters
+
+    // optional: show undistorted images
+
+    // perform pose estimation
+        // get image
+        // get 3D world positions from NN
+        // find features
+        // start calculation
+
+    // extract extrinsic parameters
+
+    // calculate transformation from reference adapter to camera sensor
+
+    // store intrinsic parameters, tracking transformation and algorithm statistics
+    return 0;
+}
